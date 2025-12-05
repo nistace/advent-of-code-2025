46a4e19e87fd521b41609859377f788e3bd217d5
nathan.istace <nathan.istace@gmail.com>
2025-12-05 17:38:53 +0100

    Day 5 Part 1

---
+++ Assets/Day05/Scripts/Day05.DayController.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Shared;
+
+namespace Day05
+{
+   public class DayController : Shared.DayController
+   {
+      protected override int DayNumber => 5;
+
+      private void ReadInput(out HashSet<LongRange> freshRanges, out long[] values)
+      {
+         var inputLines = ReadInputLines();
+         freshRanges = new HashSet<LongRange>();
+
+         var inputFreshRanges = inputLines.TakeWhile(t => !string.IsNullOrEmpty(t))
+            .Select(t => t.Split('-'))
+            .Select(t => new LongRange(long.Parse(t[0]), long.Parse(t[1])))
+            .ToArray();
+
+         foreach (var nextRange in inputFreshRanges)
+         {
+            var overlappingRanges = freshRanges.Where(nextRange.IsOverlappingOrAdjacent).ToList();
+            freshRanges.ExceptWith(overlappingRanges);
+            overlappingRanges.Add(nextRange);
+            freshRanges.Add(LongRange.MergeOverlappingRanges(overlappingRanges));
+         }
+
+         values = inputLines.Skip(inputFreshRanges.Length + 1).ToArray().Select(long.Parse).ToArray();
+      }
+
+      protected override async UniTask<string> SolvePart1(CancellationToken cancellationToken)
+      {
+         ReadInput(out var freshRanges, out var ingredients);
+
+         var result = ingredients.Count(ingredient => freshRanges.Any(range => range.Contains(ingredient)));
+
+         return await UniTask.FromResult($"{result}");
+      }
+
+      protected override async UniTask<string> SolvePart2(CancellationToken cancellationToken)
+      {
+         return await UniTask.FromResult("");
+      }
+   }
+}
---
+++ Assets/Shared/Scripts/LongRange.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shared
+{
+   public readonly struct LongRange : IEquatable<LongRange>
+   {
+      private long Min { get; }
+      private long Max { get; }
+
+      public LongRange(long min, long max)
+      {
+         Min = min;
+         Max = max;
+      }
+
+      // Check overlaps in the range, also returns true 
+      public bool IsOverlappingOrAdjacent(LongRange other)
+      {
+         if (Contains(other.Min)) return true;
+         if (Contains(other.Max)) return true;
+         if (other.Contains(Min)) return true;
+         if (other.Contains(Max)) return true;
+         if (other.Min == Max + 1) return true;
+         if (other.Max == Min - 1) return true;
+
+         return false;
+      }
+
+      public bool Contains(long value) => value >= Min && value <= Max;
+
+      public static LongRange MergeOverlappingRanges(List<LongRange> overlappingRanges) => new(overlappingRanges.Min(t => t.Min), overlappingRanges.Max(t => t.Max));
+
+      public bool Equals(LongRange other) => Min == other.Min && Max == other.Max;
+      public override bool Equals(object obj) => obj is LongRange other && Equals(other);
+      public override int GetHashCode() => HashCode.Combine(Min, Max);
+   }
+}
