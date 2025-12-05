9c89d65462a01631322c37a1bc3c360441fbbc39
nathan.istace <nathan.istace@gmail.com>
2025-12-05 16:37:48 +0100

    Day 4 Part 2

--- Assets/Day04/Scripts/Day04.DayController.cs
+++ Assets/Day04/Scripts/Day04.DayController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Cysharp.Threading.Tasks;
@@ -9,27 +10,37 @@ namespace Day04
    {
       protected override int DayNumber => 4;
 
+      private readonly Vector2Int[] _directions = { new(-1, -1), new(-1, 0), new(-1, 1), new(0, -1), new(0, 1), new(1, -1), new(1, 0), new(1, 1) };
+      private const int MAX_NEIGHBOURS = 3;
+
+      private HashSet<Vector2Int> ReadInputCoordinatesHashSet() => ReadInputLines()
+         .SelectMany((line, y) => line.Select((c, x) => (value: c, coordinates: new Vector2Int(x, y))))
+         .Where(t => t.value == '@')
+         .Select(t => t.coordinates)
+         .ToHashSet();
+
       protected override async UniTask<string> SolvePart1(CancellationToken cancellationToken)
       {
-         await UniTask.SwitchToMainThread();
-
-         Vector2Int[] directions = { new(-1, -1), new(-1, 0), new(-1, 1), new(0, -1), new(0, 1), new(1, -1), new(1, 0), new(1, 1) };
-         const int maxNeighbours = 3;
-
-         var allRollsCoordinates = ReadInputLines()
-            .SelectMany((line, y) => line.Select((c, x) => (value: c, coordinates: new Vector2Int(x, y))))
-            .Where(t => t.value == '@')
-            .Select(t => t.coordinates)
-            .ToHashSet();
-
-         var result = allRollsCoordinates.Count(cell => directions.Count(offset => allRollsCoordinates.Contains(cell + offset)) <= maxNeighbours);
+         var allRollsCoordinates = ReadInputCoordinatesHashSet();
+         var result = allRollsCoordinates.Count(cell => _directions.Count(offset => allRollsCoordinates.Contains(cell + offset)) <= MAX_NEIGHBOURS);
 
          return await UniTask.FromResult($"{result}");
       }
 
       protected override async UniTask<string> SolvePart2(CancellationToken cancellationToken)
       {
-         return await UniTask.FromResult("");
+         var remainingRollsCoordinates = ReadInputCoordinatesHashSet();
+
+         var removedInTotal = 0;
+         int removedLastTime;
+
+         do
+         {
+            removedLastTime = remainingRollsCoordinates.RemoveWhere(cell => _directions.Count(offset => remainingRollsCoordinates.Contains(cell + offset)) <= MAX_NEIGHBOURS);
+            removedInTotal += removedLastTime;
+         } while (removedLastTime > 0);
+
+         return await UniTask.FromResult($"{removedInTotal}");
       }
    }
 }
