f61e51d9631d2d90581edbddd4d6f7e242ace86f
nathan.istace <nathan.istace@gmail.com>
2025-12-06 10:23:58 +0100

    Day 6 part 2

--- Assets/Day06/Scripts/Day06.DayController.cs
+++ Assets/Day06/Scripts/Day06.DayController.cs
@@ -9,20 +9,15 @@ namespace Day06
    {
       protected override int DayNumber => 6;
 
-      private void ReadInput(out (long[] operands, char @operator)[] operations)
+      protected override async UniTask<string> SolvePart1(CancellationToken cancellationToken)
       {
          var splitLines = ReadInputLines().Select(t => t.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToArray();
          var operators = splitLines[^1];
          Array.Resize(ref splitLines, splitLines.Length - 1);
 
-         operations = Enumerable.Range(0, splitLines[0].Length)
+         var operations = Enumerable.Range(0, splitLines[0].Length)
             .Select(index => (operands: splitLines.Select(t => long.Parse(t[index])).ToArray(), @operator: operators[index][0]))
             .ToArray();
-      }
-
-      protected override async UniTask<string> SolvePart1(CancellationToken cancellationToken)
-      {
-         ReadInput(out var operations);
 
          var sum = operations.Where(t => t.@operator == '+').Sum(t => t.operands.Sum()) +
                    operations.Where(t => t.@operator == '*').Sum(t => t.operands.Aggregate(1L, (current, other) => current * other));
@@ -32,8 +27,42 @@ namespace Day06
 
       protected override async UniTask<string> SolvePart2(CancellationToken cancellationToken)
       {
-         ReadInput(out _);
-         return await UniTask.FromResult("");
+         var operandsLines = ReadInputLines().Select(t => t).ToArray();
+         var operatorsLine = operandsLines[^1];
+         Array.Resize(ref operandsLines, operandsLines.Length - 1);
+
+         var sum = 0L;
+         var index = 0;
+         var multiplying = false;
+         var operationBuffer = 0L;
+
+         while (index < operatorsLine.Length)
+         {
+            if (operatorsLine[index] != ' ')
+            {
+               sum += operationBuffer;
+               multiplying = operatorsLine[index] == '*';
+               operationBuffer = multiplying ? 1 : 0;
+            }
+
+            if (long.TryParse(string.Join("", operandsLines.Select(t => t[index])).Trim(), out var operand))
+            {
+               if (multiplying)
+               {
+                  operationBuffer *= operand;
+               }
+               else
+               {
+                  sum += operand;
+               }
+            }
+
+            index++;
+         }
+
+         sum += operationBuffer;
+
+         return await UniTask.FromResult($"{sum}");
       }
    }
 }
