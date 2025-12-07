9327e2e0c2880a7529a491eb56ef7f81658fae05
nathan.istace <nathan.istace@gmail.com>
2025-12-07 08:18:37 +0100

    Day 07 Part 2

--- Assets/Day07/Scripts/Day07.DayController.cs
+++ Assets/Day07/Scripts/Day07.DayController.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using UnityEngine;
 
 namespace Day07
 {
@@ -36,7 +35,37 @@ namespace Day07
 
       protected override string SolvePart2()
       {
-         return "";
+         const long splitterValue = -1L;
+         var map = ReadInputLines()
+            .Select(line => line.Select(character => character switch
+               {
+                  '^' => splitterValue,
+                  'S' => 1,
+                  _ => 0
+               })
+               .ToArray())
+            .ToArray();
+
+         var height = map.Length;
+         var width = map[0].Length;
+
+         for (var y = 1; y < height; ++y)
+         {
+            for (var x = 0; x < width; ++x)
+            {
+               if (map[y][x] == splitterValue && map[y - 1][x] > 0)
+               {
+                  map[y][x - 1] += map[y - 1][x];
+                  map[y][x + 1] += map[y - 1][x];
+               }
+               else if (map[y - 1][x] > 0)
+               {
+                  map[y][x] += map[y - 1][x];
+               }
+            }
+         }
+
+         return $"{map[height - 1].Sum()}";
       }
    }
 }
