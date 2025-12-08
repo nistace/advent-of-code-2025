e25bf02c77fd278b1fb609fc8fb803eb88f0811a
nathan.istace <nathan.istace@gmail.com>
2025-12-08 09:52:30 +0100

    Day 8 Part 2

--- Assets/Day08/Scripts/Day08.DayController.cs
+++ Assets/Day08/Scripts/Day08.DayController.cs
@@ -81,7 +81,74 @@ namespace Day08
 
       protected override string SolvePart2()
       {
-         return "";
+         var allCoordinates = ReadInputLines()
+            .Select(t => t.Split(','))
+            .Select(t => new Vector3Int(int.Parse(t[0]), int.Parse(t[1]), int.Parse(t[2])))
+            .OrderBy(t => t.x)
+            .ThenBy(t => t.y)
+            .ThenBy(t => t.z)
+            .ToArray();
+
+         var nodeCount = allCoordinates.Length;
+
+         var distances = allCoordinates.SelectMany((first, index) =>
+               allCoordinates.Skip(index + 1).Select(second => (from: first, to: second, sqrMagnitude: Vector3.SqrMagnitude(first - second))))
+            .OrderBy(t => t.sqrMagnitude)
+            .ToArray();
+
+         var circuitIndicesPerNode = new Dictionary<Vector3Int, int>();
+         var circuits = new List<HashSet<Vector3Int>>();
+         var lastModifiedCircuitSize = 0;
+         (Vector3Int from, Vector3Int to, float sqrMagnitude) lastConnection = default;
+
+         for (var distanceIndex = 0; distanceIndex < distances.Length && lastModifiedCircuitSize < nodeCount; ++distanceIndex)
+         {
+            lastConnection = distances[distanceIndex];
+
+            var fromIsConnected = circuitIndicesPerNode.TryGetValue(lastConnection.from, out var fromCircuit);
+            var toIsConnected = circuitIndicesPerNode.TryGetValue(lastConnection.to, out var toCircuit);
+
+            if (fromIsConnected && toIsConnected && fromCircuit == toCircuit)
+            {
+               // Both are already connected together
+            }
+            else if (fromIsConnected && toIsConnected)
+            {
+               // Both are part of distinct circuits => we merge them
+               foreach (var toCircuitItem in circuits[toCircuit])
+               {
+                  circuits[fromCircuit].Add(toCircuitItem);
+                  circuitIndicesPerNode[toCircuitItem] = fromCircuit;
+               }
+
+               circuits[toCircuit].Clear();
+               lastModifiedCircuitSize = circuits[fromCircuit].Count;
+            }
+            else if (fromIsConnected)
+            {
+               circuits[fromCircuit].Add(lastConnection.to);
+               circuitIndicesPerNode[lastConnection.to] = fromCircuit;
+               lastModifiedCircuitSize = circuits[fromCircuit].Count;
+            }
+            else if (toIsConnected)
+            {
+               circuits[toCircuit].Add(lastConnection.from);
+               circuitIndicesPerNode[lastConnection.from] = toCircuit;
+               lastModifiedCircuitSize = circuits[toCircuit].Count;
+            }
+            else
+            {
+               var newCircuitIndex = circuits.Count;
+               circuits.Add(new HashSet<Vector3Int> { lastConnection.from, lastConnection.to });
+               circuitIndicesPerNode[lastConnection.from] = newCircuitIndex;
+               circuitIndicesPerNode[lastConnection.to] = newCircuitIndex;
+               lastModifiedCircuitSize = 2;
+            }
+         }
+
+         var result = lastConnection.from.x * lastConnection.to.x;
+
+         return $"{result}";
       }
    }
 }
